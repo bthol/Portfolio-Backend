01bef89a9c7e21cfa68454b2ccdb5843cb682c40
bthol <95544562+bthol@users.noreply.github.com>
2022-06-01 10:00:57 -0700

    nodemon update

--- App.js
+++ App.js
@@ -2,32 +2,31 @@
 
 // Dendencies
 const express = require("express");
-const app = express();
+const server = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require('body-parser');
 
 //Middleware
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+server.use(express.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(cors());
 
 //Controller indices
 const control1 = require("./Controls/control1");
-app.use(`/data`, control1);
+server.use(`/data`, control1);
 
 //landing Route
-app.get(`/`, (req, res) => {
-    // res.send("land index is active")
+server.get(`/`, (req, res) => {
     res.redirect(`/data`)
 })
 
-//Database Test
+//Database Error Handling
 mongoose.connection.on(`error`, (error) => console.error(error));
 mongoose.connection.once(`open`, () => console.log("MongoDB connected"));
 
-module.exports = app.listen(3000, () => {
-    console.log("express is online and running")
+module.exports = server.listen(3000, () => {
+    console.log("Express.js is online and running")
 });
 
 //end line
