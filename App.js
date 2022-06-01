6f6f4a8bfa773e770f572470d7bddebf73ac7121
bthol <95544562+bthol@users.noreply.github.com>
2022-06-01 09:47:14 -0700

    database error handling

--- App.js
+++ App.js
@@ -4,23 +4,27 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const cors = require("cors")
+const cors = require("cors");
 const bodyParser = require('body-parser');
 
 //Middleware
-app.use(express.json())
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors());
 
 //Controller indices
-const control1 = require("./Controls/control1")
-app.use(`/data`, control1)
+const control1 = require("./Controls/control1");
+app.use(`/data`, control1);
 
 //landing Route
 app.get(`/`, (req, res) => {
     // res.send("land index is active")
     res.redirect(`/data`)
 })
+
+//Database Test
+mongoose.connection.on(`error`, (error) => console.error(error));
+mongoose.connection.once(`open`, () => console.log("MongoDB connected"));
 
 module.exports = app.listen(3000, () => {
     console.log("express is online and running")
--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Dependencies (listed in install order):
 NOTE: Features liable to change.
 
 Created     : 02/24/2022
-Last update : 04/20/2022
+Last update : 06/01/2022
 -----------------------------------------------
 Version     |   Date        |   Description
 0.1         |   02/24/2022  |   Project Setup (i.e. Directories, file structure, files)
